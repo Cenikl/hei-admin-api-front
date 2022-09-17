a81d43b55badd521575b371ec65b420dec395fd6
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-09-17 06:22:53 +0300

    feat: add select option coponent

---
+++ src/components/select/Select.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FC, ChangeEvent } from 'react';
+import { selectPropType } from "../../type";
+import './Select.css'
+
+const Input: FC<Partial<selectPropType>> = (props) => {
+    const {available, defaultValue, collect, label} = props;
+
+    return (
+        <>
+            <div className='select__label'>{label}</div>
+            <select onChange={(e)=>{{collect && collect(e.target.value)}}} 
+                className="select__element"
+                >
+                {available && 
+                    available.map((element)=>(
+                        <option value={element} 
+                                key={element}
+                                selected={(defaultValue === element && true)}
+                                >
+                            {element}
+                        </option>
+                    ))
+                }
+            </select>
+        </>
+    )
+}
+
+
+export default Input
